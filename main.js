110ab8f254cfd92c609eeb842160f60798ff6148
WaheedAhmed74 <159169508+WaheedAhmed74@users.noreply.github.com>
2024-04-01 21:25:12 +0500

    To Do List

---
+++ main.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todo = [];
+let flag = true;
+while (flag) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "toDo",
+            message: "what you want to add in to Do?",
+            type: "input"
+        },
+        {
+            name: "AddMore",
+            type: "confirm",
+            Message: "Do you want to add more",
+            default: "false"
+        }
+    ]);
+    todo.push(addTask.toDo);
+    flag = addTask.AddMore;
+    console.log(todo);
+}
